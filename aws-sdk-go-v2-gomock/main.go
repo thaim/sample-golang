51fe4572d2efa39313b576726236ca77e31eb916
thaim <thaim24@gmail.com>
2022-10-16 14:50:24 +0000

    copy sample code from official docs

---
+++ aws-sdk-go-v2-gomock/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// S3GetObjectAPI is
+type S3GetObjectAPI interface {
+	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+}
+
+// GetObjectFromS3 returns S3 Object body
+func GetObjectFromS3(ctx context.Context, api S3GetObjectAPI, bucket, key string) ([]byte, error) {
+	object, err := api.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &bucket,
+		Key:    &key,
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer object.Body.Close()
+
+	return ioutil.ReadAll(object.Body)
+}
