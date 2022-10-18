dfdfd54871eee5178048dbc1f7726c709e6b6a5b
thaim <thaim24@gmail.com>
2022-10-18 14:50:20 +0000

    implement main func to read object using aws-sdk-go-v2

--- aws-sdk-go-mockinterface/go.mod
+++ aws-sdk-go-mockinterface/go.mod
@@ -2,4 +2,8 @@ module github.com/thaim/sample-golang/aws-sdk-go-mockinterface
 
 go 1.16
 
-require github.com/aws/aws-sdk-go-v2/service/s3 v1.27.11 // indirect
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.16 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.17.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.27.11 // indirect
+)
--- aws-sdk-go-mockinterface/main.go
+++ aws-sdk-go-mockinterface/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
+	"log"
 
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-// S3GetObjectAPI is
+// S3GetObjectAPI interfaceを定義し、プロダクションコードかテストかに応じて実装をDIできるようにする
 type S3GetObjectAPI interface {
 	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 }
 
-// GetObjectFromS3 returns S3 Object body
+// GetObjectFromS3 は S3GetObjectAPI interfaceを利用する。 api.GetObjectを呼び出せばよいことだけを知っている
 func GetObjectFromS3(ctx context.Context, api S3GetObjectAPI, bucket, key string) ([]byte, error) {
 	object, err := api.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: &bucket,
@@ -24,4 +27,30 @@ func GetObjectFromS3(ctx context.Context, api S3GetObjectAPI, bucket, key string
 	defer object.Body.Close()
 
 	return ioutil.ReadAll(object.Body)
+}
+
+
+func main() {
+	var bucket = "sample-bucket-thaim"
+	var key = "sample-object-key"
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+	params := &s3.GetObjectInput{
+		Bucket: &bucket,
+		Key: &key,
+	}
+
+	object, err := client.GetObject(context.Background(), params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer object.Body.Close()
+
+	objectString, _ := ioutil.ReadAll(object.Body)
+	fmt.Println(string(objectString))
 }
