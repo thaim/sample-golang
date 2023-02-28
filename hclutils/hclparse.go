465ce52d59b1ea091932604a57e0ddab7c82e7ab
thaim <thaim24@gmail.com>
2023-02-28 23:43:07 +0900

    add sample for parseHCL, Files and Sources in hclparse

--- hclutils/hclparse.go
+++ hclutils/hclparse.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/hashicorp/hcl/v2/hclparse"
 )
@@ -15,6 +17,46 @@ func main() {
 		fmt.Println(diags.Error())
 		return
 	}
-
 	fmt.Printf("%s", file.Bytes)
+
+
+	// hclパッケージを用いず予めバッファにファイルを読み込んでおく
+	var body []byte
+	f, err := os.Open("sample2.hcl")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	body, err = io.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// ParseHCLに読み込み済のバッファとその仮想ファイル名を与える
+	file, diags = p.ParseHCL(body, "sampleX.hcl")
+	if diags.HasErrors() {
+		fmt.Println(diags.Error())
+		return
+	}
+	fmt.Printf("%s", file.Bytes)
+
+
+	// Filesではファイル名とコンテンツのmapが返る
+	mapping := p.Files()
+	for k, v := range mapping {
+		fmt.Printf("filename: %s\n", k)
+		fmt.Printf("contents: %s\n", v.Bytes)
+	}
+
+
+	// Sourcesを使えばhcl.Fileを経由せずにコンテンツを取得できる
+	// これはパースエラー時に利用する(hcl.Fileでは不正なファイルを表現できないので)
+	src := p.Sources()
+	for k, v := range src {
+		fmt.Printf("filename: %s\n", k)
+		fmt.Printf("contents: %s\n", v)
+	}
 }
