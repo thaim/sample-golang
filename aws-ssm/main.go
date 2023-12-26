087decd59895f1f9133987664cd4e7f72882eda5
thaim <thaim24@gmail.com>
2023-12-26 23:30:00 +0900

    get value from specified parameter store

--- aws-ssm/main.go
+++ aws-ssm/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -10,13 +11,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Get data from SSM: ")
+	params := "sample"
+	if len(os.Args) == 2 {
+		params = os.Args[1]
+	}
+
+	fmt.Println("Get data from SSM: " + params)
 
 	cfg, _ := config.LoadDefaultConfig(context.TODO())
 	api := ssm.NewFromConfig(cfg)
 
 	output, _ := api.GetParameter(context.TODO(), &ssm.GetParameterInput{
-		Name:           aws.String("sample"),
+		Name:           aws.String(params),
 		WithDecryption: aws.Bool(true),
 	})
 
